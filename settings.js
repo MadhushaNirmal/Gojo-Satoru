a432a426a9a5658be0bea5593b414a0887798085
MadhushaNirmal <110528609+MadhushaNirmal@users.noreply.github.com>
2022-08-03 14:16:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94775949304']
+global.premium = ['94775949304']
+global.ownernomer = '94775949304'
+global.ownername = '𝙼𝙰𝙳𝚄𝚂𝙷𝙰 𝙽𝙸𝚁𝙼𝙰𝙻'
+global.botname = '𝘕𝘐𝘔𝘈 𝘉𝘖𝘛'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = '𝘕𝘐𝘔𝘈 𝘉𝘖𝘛'
+global.author = '🐦 ＮＩＭＡ'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
